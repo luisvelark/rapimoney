fae31cf0161781f9ef5e94fe8608667923218894
luisito <velark07@gmail.com>
2021-02-22 20:02:06 -0300

    iniciando preview

---
+++ app/Views/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>rapimoney</title>
+
+  <!-- NORMALIZE -->
+  <link rel="stylesheet" href="<?php echo base_url('/assets/normalize.css') ?>">
+
+  <!-- OWN STYLES -->
+  <link rel="stylesheet" href="<?php echo base_url('/assets/style.css') ?>">
+</head>
+
+<body>
+  <!--TOP IMAGES! -->
+  <header>
+
+
+
+  </header>
+
+  <!-- MAIN -->
+  <section>
+
+  </section>
+
+  <!--FOOTER! -->
+  <footer>
+
+  </footer>
+
+
+
+</body>
+
+</html>
