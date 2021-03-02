2bc41f8ba52410259b2c349e80d5a247d2b6977b
luisito <velark07@gmail.com>
2021-03-02 20:19:43 -0300

    version movil interfaz finalizado

--- app/Views/home.php
+++ app/Views/home.php
@@ -8,13 +8,13 @@
   <title>rapimoney</title>
 
   <!-- NORMALIZE -->
-  <link rel="stylesheet" href="<?php echo base_url('/assets/normalize.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('/assets/style/normalize.css') ?>">
 
   <!-- BULMA CSS -->
-  <link rel="stylesheet" href="<?php echo base_url('/assets/bulma css/bulma.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('/assets/bulma css/bulma.min.css') ?>">
 
   <!-- OWN STYLES -->
-  <link rel="stylesheet" href="<?php echo base_url('/assets/style.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('/assets/style/style.css') ?>">
 
 
 
@@ -24,22 +24,73 @@
   <!--TOP IMAGES! -->
   <header>
 
-    <!-- <div>
-      <p>hola</p>
-    </div> -->
+    <div class="pt-3">
+      <div class="is-flex is-justify-content-space-around">
 
+        <img class="" src="<?php echo base_url('/assets/img/header/pago.png') ?>" alt="pago">
 
+        <img class="" src="<?php echo base_url('/assets/img/header/virtual.png') ?>" alt="virtual">
 
+        <img class="" src="<?php echo base_url('/assets/img/header/rapido.png') ?>" alt="rapido">
+
+        <img class="" src="<?php echo base_url('/assets/img/header/virtual.png') ?>" alt="virtual">
+
+        <img class="" src="<?php echo base_url('/assets/img/header/virtual.png') ?>" alt="virtual">
+
+        <img class="" src="<?php echo base_url('/assets/img/header/rapido.png') ?>" alt="rapido">
+
+        <img class="" src="<?php echo base_url('/assets/img/header/virtual.png') ?>" alt="virtual">
+
+        <img class="" src="<?php echo base_url('/assets/img/header/pago.png') ?>" alt="pago">
+
+      </div>
+    </div>
   </header>
 
-  <!-- MAIN -->
-  <section>
+  <!-- PROMO RAPIMONEY! -->
+  <main class="mt-6 mb-0">
 
-  </section>
+    <!-- SECCION DE MUY PRONTO -->
+    <section class="pt-5 my-5">
+      <p class="is-size-5 is-italic has-text-weight-bold has-text-white ml-5">MUY PRONTO...</p>
+      <hr class="mt-0 mb-5">
+    </section>
 
-  <!--FOOTER! -->
-  <footer>
+    <!-- SECCION LOGO -->
+    <section class="pt-5 my-5">
+      <div class="content-logo my-5">
+        <img class="logo my-5" src="<?php echo base_url('/assets/img/main/logo.png') ?>" alt="logo">
+      </div>
+    </section>
 
+    <section class="my-6 is-flex is-justify-content-center">
+      <button class="button is-small is-rounded btn has-text-white my-1">GANA AQUÍ</button>
+    </section>
+
+    <section>
+      <div class="content-edificios mt-4">
+        <img class="edificio" src="<?php echo base_url('/assets/img/main/fondoedificios.png') ?>" alt="edificio">
+      </div>
+    </section>
+
+
+  </main>
+
+  <!--REDES SOCIALES! -->
+  <footer class="mt-0">
+    <div class="contactos">
+
+      <div class="item-contacto">
+        <img class="contacto" src="<?php echo base_url('/assets/img/footer/facebook.png') ?>" alt="">
+        <span class="is-size-7 has-text-white">/RapiMoney</span>
+      </div>
+
+      <div class="item-contacto">
+        <img class="contacto" src="<?php echo base_url('/assets/img/footer/whatsaap.png') ?>" alt="">
+        <span class="is-size-7 has-text-white">921 434 247</span>
+      </div>
+
+    </div>
   </footer>
 
   <script src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
